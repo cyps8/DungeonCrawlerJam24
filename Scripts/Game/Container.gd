cfabb65007d342344474a8ff46711ce87a0b795d
Cyp <cypriane81@gmail.com>
2024-03-31 03:41:20 +0100

    [+Added] Container + inv fix

--- Scripts/Game/Container.gd
+++ Scripts/Game/Container.gd
@@ -10,6 +10,8 @@ func Interact(_tile: MapTile):
 		return
 	opened = true
 	GameManager.instance.hudRef.ShowHint("Container opened")
+	var openTween: Tween = create_tween()
+	openTween.tween_property($Mesh/Hinge, "rotation:y", deg_to_rad(-30), 0.7).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
 	for item in items:
 		GameManager.instance.invRef.AddItem(item)
 		GameManager.instance.hudRef.AddAction(item.name + " added to inventory", item.icon)
